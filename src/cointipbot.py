a1bac89be03739f06ab014dc694fafb23a4cd359
vindimy <vindimy@gmail.com>
2013-07-01 16:51:48 +0000

    The bot now upvotes tips!

--- src/cointipbot.py
+++ src/cointipbot.py
@@ -269,6 +269,7 @@ class CointipBot(object):
                     if m.was_comment:
                         # Attempt to evaluate as comment / mention
                         action = ctb_action._eval_comment(m, self)
+                        m.upvote()
                     else:
                         # Attempt to evaluate as inbox message
                         action = ctb_action._eval_message(m, self)
