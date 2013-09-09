1c88c06f6caec08f5b830ff35d41d6275571f209
vindimy <vindimy@gmail.com>
2013-09-09 00:15:18 +0000

    Adding GPL v2 license

--- src/cointipbot.py
+++ src/cointipbot.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+"""
+    This file is part of ALTcointip.
+
+    ALTcointip is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ALTcointip is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ALTcointip.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from ctb import ctb_action, ctb_db, ctb_log, ctb_misc, ctb_user
 
 import gettext, locale, logging, sys, time
