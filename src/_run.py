4a88aef2158e00396d9c4058fd6bf3216a497a65
vindimy <vindimy@gmail.com>
2013-10-31 01:06:07 +0000

    Improving startup script and removing old _run.py

--- src/_run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import cointipbot
-
-cb = cointipbot.CointipBot()
-cb.main()
--- src/_start.sh
+++ src/_start.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sudo su - altcointip -c "cd `pwd` && python ./_run.py"
+user=$1
+
+if [ ! -f ./cointipbot.py ] ; then
+	# Output help message and exit
+	echo "Usage: $0 [username]"
+	echo "if [username] is specified, script will be started as user [username]"
+	echo "$0 must be run from 'src' directory of ALTcointip"
+	exit 1
+fi
+
+if [ -z "$user" ] ; then
+	# Run as current user
+	python -c 'import cointipbot; cb=cointipbot.CointipBot(); cb.main()'
+else
+	# Run as $user
+	sudo su - $user -c "cd `pwd` && python -c 'import cointipbot; cb=cointipbot.CointipBot(); cb.main()'"
+fi
