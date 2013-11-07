64eef1c3774471e12cf6e98ff6a5dab26a60af28
vindimy <vindimy@gmail.com>
2013-11-07 17:28:57 +0000

    Updating to use new var names

--- src/_add_coin.py
+++ src/_add_coin.py
@@ -1,12 +1,15 @@
 # Here's how to add a new coin type to CointipBot
 
-import cointipbot
+import cointipbot, logging
 from ctb import ctb_misc
+
+logging.basicConfig()
+lg = logging.getLogger('cointipbot')
 
 # Make sure CointipBot instance is NOT running
 
 # First, enable new coin in config.yml
 
 # Then run these two commands
-cb = cointipbot.CointipBot(self_checks=False)
-#ctb_misc._add_coin('dvc', cb._mysqlcon, cb._coins)
+ctb = cointipbot.CointipBot(self_checks=False)
+#ctb_misc.add_coin('dvc', ctb.db, ctb.coins)
