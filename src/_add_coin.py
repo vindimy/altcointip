536c6db8b06d72e17a8d883299fb9335a0a96e6d
vindimy <vindimy@gmail.com>
2013-12-18 06:15:02 +0000

    Improving code

--- src/_add_coin.py
+++ src/_add_coin.py
@@ -1,7 +1,14 @@
 # Here's how to add a new coin type to CointipBot
 
-import cointipbot, logging
-from ctb import ctb_misc
+import cointipbot, logging, sys
+from ctb import ctb_coin, ctb_misc
+
+if not len(sys.argv) == 2:
+        print "Usage: %s COIN" % sys.argv[0]
+        print "(COIN refers to ctb.conf[COIN], a hash location in coins.yml)"
+        sys.exit(1)
+
+coin = sys.argv[1]
 
 logging.basicConfig()
 lg = logging.getLogger('cointipbot')
@@ -11,6 +18,7 @@ lg.setLevel(logging.DEBUG)
 
 # First, enable new coin in config.yml
 
-# Then run these two commands
-ctb = cointipbot.CointipBot(self_checks=False)
-#ctb_misc.add_coin('dvc', ctb.db, ctb.coins)
+# Then run these commands
+ctb = cointipbot.CointipBot(self_checks=False, init_reddit=False, init_coins=False, init_exchanges=False, init_db=True, init_logging=True)
+ctb.coins[coin] = ctb_coin.CtbCoin(_conf=ctb.conf.coins[coin])
+ctb_misc.add_coin(coin, ctb.db, ctb.coins)
